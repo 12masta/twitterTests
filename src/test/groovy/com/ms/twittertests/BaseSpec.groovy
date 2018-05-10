6afedae343c8645b4c9421fea0620bcc47f5485c
marcin.stanek <c4e6041e2ad4fa41452caf5b4801dc32>
2018-05-10 15:52:27 +0200

    TweetSpec for api integration implemented

--- src/test/groovy/com/ms/twittertests/BaseSpec.groovy
+++ src/test/groovy/com/ms/twittertests/BaseSpec.groovy
@@ -1,11 +1,15 @@
 package com.ms.twittertests
 
+import com.ms.twittertests.api.TweetTimeline
 import com.ms.twittertests.credentials.User
 import com.ms.twittertests.credentials.UserImpl
 import com.ms.twittertests.data.DummyUser
+import com.ms.twittertests.http.HttpClientImpl
+import com.ms.twittertests.http.OauthResolver
 import com.ms.twittertests.pages.HomePage
 import com.ms.twittertests.pages.LogInPageWebImpl
 import io.github.bonigarcia.wdm.WebDriverManager
+import okhttp3.OkHttpClient
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import spock.lang.Shared
@@ -17,13 +21,19 @@ class BaseSpec extends Specification {
     WebDriver driver
 
     @Shared
-    User validUser = new UserImpl(DummyUser.BASE_USER.login, DummyUser.BASE_USER.password)
+    User validUser = new UserImpl(
+            DummyUser.BASE_USER.login,
+            DummyUser.BASE_USER.password,
+            DummyUser.BASE_USER.tweeterId,
+            DummyUser.BASE_USER.name)
+
+    def tweetTimeline = new TweetTimeline(new HttpClientImpl(new OauthResolver(), new OkHttpClient()))
 
     @Shared
             generator = { int n ->
                 new Random().with {
                     String alphabet = (('A'..'Z') + ('0'..'9')).join()
-                    (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+                    (1..n).collect { alphabet[nextInt(alphabet.length())] }.join()
                 }
             }
 
--- src/test/groovy/com/ms/twittertests/tweet/TweetSpec.groovy
+++ src/test/groovy/com/ms/twittertests/tweet/TweetSpec.groovy
@@ -35,4 +35,50 @@ class TweetSpec extends BaseSpec {
         tweetBody     | user
         generator(10) | validUser
     }
+
+    @Unroll
+    TestCase "User post a tweet using timeline and tweet appears in the api"() {
+        given: "timeline has specified size"
+        def numbersOfTweetsBefore = tweetTimeline.getUserTimeline(user).size()
+
+        when: "user log in"
+        def homePage = logIn user
+        and: "post a tweet using timeline"
+        homePage.postTweetUsingTimeline tweetBody
+
+        then: "the number of tweets is higher by 1"
+        def tweets = tweetTimeline.getUserTimelineAwaitable user, numbersOfTweetsBefore + 1
+        tweets.size() == numbersOfTweetsBefore + 1
+        and: "tweet has correct body"
+        tweets.first().text == tweetBody
+        and: "tweet has a proper author"
+        tweets.first().user.name == validUser.name
+
+        where:
+        tweetBody    | user
+        generator(9) | validUser
+    }
+
+    @Unroll
+    TestCase "User post a tweet using navbar and tweet appears in the api"() {
+        given: "timeline has specified size"
+        def numbersOfTweetsBefore = tweetTimeline.getUserTimeline(user).size()
+
+        when: "user log in"
+        def homePage = logIn user
+        and: "post a tweet using timeline"
+        homePage.postTweetUsingNavBar tweetBody
+
+        then: "the number of tweets is higher by 1"
+        def tweets = tweetTimeline.getUserTimelineAwaitable user, numbersOfTweetsBefore + 1
+        tweets.size() == numbersOfTweetsBefore + 1
+        and: "tweet has correct body"
+        tweets.first().text == tweetBody
+        and: "tweet has a proper author"
+        tweets.first().user.name == validUser.name
+
+        where:
+        tweetBody     | user
+        generator(10) | validUser
+    }
 }
