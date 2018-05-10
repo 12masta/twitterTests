2ac7ec0e18b6dd61822ed10fade16268d9b28ca7
marcin.stanek <c4e6041e2ad4fa41452caf5b4801dc32>
2018-05-11 01:16:52 +0200

    Http classes refactor for proper OAuth handling

--- src/main/java/com/ms/twittertests/http/HttpClient.java
+++ src/main/java/com/ms/twittertests/http/HttpClient.java
@@ -1,7 +1,13 @@
 package com.ms.twittertests.http;
 
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+
 import java.io.IOException;
 
 public interface HttpClient {
-    String get(String url) throws IOException;
+    String get(String url) throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException;
+
+    String post(String url, String json) throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException;
 }
--- src/main/java/com/ms/twittertests/http/HttpClientImpl.java
+++ src/main/java/com/ms/twittertests/http/HttpClientImpl.java
@@ -1,27 +1,49 @@
 package com.ms.twittertests.http;
 
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 
 import java.io.IOException;
 
-public class HttpClientImpl implements HttpClient{
+public class HttpClientImpl implements HttpClient {
     private OkHttpClient client;
-    private OauthResolver oauthResolver;
+    private HttpData httpData;
 
-    public HttpClientImpl(OauthResolver oauthResolver, OkHttpClient client) {
+    public HttpClientImpl(HttpData httpData, OkHttpClient client) {
         this.client = client;
-        this.oauthResolver = oauthResolver;
+        this.httpData = httpData;
     }
 
-    public String get(String url) throws IOException {
+    public String get(String url) throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException {
         Request request = new Request.Builder()
                 .url(url)
-                .addHeader(oauthResolver.getHeaderName(), oauthResolver.getHeaderValue())
                 .build();
-
-        Response response = client.newCall(request).execute();
+        Request signedRequest = (Request) getConsumer().sign(request).unwrap();
+        Response response = client.newCall(signedRequest).execute();
         return response.body().string();
+    }
+
+    @Override
+    public String post(String url, String json) throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException {
+        RequestBody body = RequestBody.create(null, new byte[0]);
+        Request request = new Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .build();
+        Request signedRequest = (Request) getConsumer().sign(request).unwrap();
+        Response response = client.newCall(signedRequest).execute();
+        return response.body().string();
+    }
+
+    private OkHttpOAuthConsumer getConsumer() {
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(httpData.getConsumerKey(), httpData.getConsumerSecret());
+        consumer.setTokenWithSecret(httpData.getAccessToken(), httpData.getAccessTokenSecret());
+        return consumer;
     }
 }
